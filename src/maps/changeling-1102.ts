1077106bd6a0081f4b264fbe58a88e5002d3ac97
David Kanekanian <dkanekanian1@gmail.com>
2020-09-02 09:30:12 +0100

    Add camera shot 1, allow multiple keys per point

--- src/maps/changeling-1102.ts
+++ src/maps/changeling-1102.ts
@@ -89,18 +89,31 @@ export class ChangelingWorld extends FolkWorldBase {
 
         // Add shots.
         this.timeline.points = [
-            // temp positioning
-            {
+            // initial position: shots A and B
+            { keys: [{
                 actorID: "camera",
                 loc: new THREE.Vector3(0, 100, -150),
                 rot: new THREE.Euler(0, Math.PI, 0)
-            },
+            }]},
 
-            {
+            // Shot 1 - 1
+            { keys: [{
                 actorID: "camera",
-                loc: new THREE.Vector3(0, 100, -150),
-                rot: new THREE.Euler(0, Math.PI, 0)
-            },
+                loc: new THREE.Vector3(61, 130, 630),
+                rot: new THREE.Euler(-0.1, 6.8, 0.05)
+            }]},
+            // Shot 1 - 2
+            { keys: [{
+                actorID: "camera",
+                loc: new THREE.Vector3(18, 95, 168),
+                rot: new THREE.Euler(-0.2, 6.5, 0)
+            }]},
+            // Shot 1 - 3
+            { keys: [{
+                actorID: "camera",
+                loc: new THREE.Vector3(18, 95, 168),
+                rot: new THREE.Euler(-0.2, 6.5, 0)
+            }]},
         ];
 
         if (this.noLogo) {
