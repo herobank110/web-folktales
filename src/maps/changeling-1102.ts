b2fb957223bab60527cd1ab93ad20b3132151d64
David Kanekanian <dkanekanian1@gmail.com>
2020-09-13 20:21:33 +0100

    Add shots 2-4 via shotlist data, no dialogue

--- src/maps/changeling-1102.ts
+++ src/maps/changeling-1102.ts
@@ -104,6 +104,8 @@ export class ChangelingWorld extends FolkWorldBase {
         // Add shots.
         const vec = THREE.Vector3;
         const rot = THREE.Euler;
+        const hide = (actorID: string) => ({ actorID, visible: false });
+        const show = (actorID: string) => ({ actorID, visible: true });
         this.timeline.points = [
             // initial position: shots A and B
             {
@@ -111,16 +113,18 @@ export class ChangelingWorld extends FolkWorldBase {
                     { actorID: "camera", loc: new vec(0, 100, -150), rot: new rot(0, Math.PI) },
                     { actorID: "cradle", loc: new vec(43, 0, 83), rot: new rot(0, -1) },
                     { actorID: "baby", loc: new vec(22, 13, 86), rot: new rot(-1.2, 0.01, -1.4) },
-                    // hide all the elf poses at the start
-                    // { actorID: "elf1_pose_a", visible: false },
-                    // { actorID: "elf2_pose_a", visible: false },
-                    // { actorID: "elf3_pose_a", visible: false },
-                    // { actorID: "elf1_pose_neutral", visible: false },
-                    // { actorID: "elf2_pose_neutral", visible: false },
-                    // { actorID: "elf3_pose_neutral", visible: false },
-                    { actorID: "mother_pose_neutral", visible: false },
-                    { actorID: "neighbor_pose_neutral", visible: false },
-                    { actorID: "mother_pose_kneel", visible: false }
+                    // hide the actors that come in later
+                    hide("elf1_pose_a"),
+                    hide("elf2_pose_a"),
+                    hide("elf3_pose_a"),
+                    hide("elf1_pose_neutral"),
+                    hide("elf2_pose_neutral"),
+                    hide("elf3_pose_neutral"),
+                    hide("mother_pose_neutral"),
+                    hide("neighbor_pose_neutral"),
+                    hide("mother_pose_kneel"),
+                    hide("elf_hand"),
+                    hide("changeling"),
                 ]
             },
             // Shot 1 - 1 - LS establish mise en scene
@@ -132,13 +136,134 @@ export class ChangelingWorld extends FolkWorldBase {
                 },
                 { actorID: "elf1_pose_neutral", loc: new vec() },
                 { actorID: "elf2_pose_neutral", loc: new vec(49) },
-                { actorID: "elf3_pose_neutral", loc: new vec(-49) },
-                { actorID: "elf_hand", loc: new vec(0, 50, 0) },]
+                { actorID: "elf3_pose_neutral", loc: new vec(-49) },]
             },
             // Shot 1 - 2
-            { keys: [{ actorID: "camera", loc: new vec(18, 95, 168), rot: new rot(-0.2, 6.5) }] },
+            {
+                keys: [{
+                    actorID: "camera",
+                    loc: new vec(18, 95, 168),
+                    rot: new rot(-0.2, 6.5)
+                }]
+            },
             // Shot 1 - 3 - MCU HA looking at baby over crib
-            { keys: [{ actorID: "camera", loc: new vec(43, 41, 113), rot: new rot(-0.6) }] },
+            {
+                keys: [
+                    {
+                        actorID: "camera",
+                        loc: new vec(43, 41, 113),
+                        rot: new rot(-0.6)
+                    },
+                    {
+                        actorID: "elf_hand",
+                        loc: new vec(26, 31, 71),
+                        rot: new rot(0, -0.6, 0.5),
+                        visible: true
+                    }
+                ]
+            },
+
+            // Shot 2 - 1 - LS top down of baby
+            {
+                keys: [
+                    {
+                        actorID: "camera",
+                        loc: new vec(47, 145, 79),
+                        rot: new rot(-1.6)
+                    },
+                    {
+                        actorID: "elf1_pose_neutral",
+                        loc: new vec(13, 0, 52),
+                        rot: new rot(0, 0.6),
+                        visible: true
+                    },
+                    {
+                        actorID: "elf2_pose_a",
+                        loc: new vec(-5, 0, 44),
+                        rot: new rot(0, 1.2),
+                        visible: true
+                    },
+                    {
+                        actorID: "elf3_pose_neutral",
+                        loc: new vec(-23, 0, 51),
+                        rot: new rot(0, 2.4),
+                        visible: true
+                    },
+                    hide("elf_hand"),
+                ]
+            },
+            // Shot 2 - 2 - MS elves climbing into cot
+            {
+                keys: [
+                    {
+                        actorID: "camera",
+                        loc: new vec(21, 55, 96),
+                        rot: new rot(-1)
+                    },
+                    {
+                        actorID: "elf1_pose_neutral",
+                        loc: new vec(16.4, 19, 56),
+                        rot: new rot(0.7, 0.5)
+                    },
+                    {
+                        actorID: "elf2_pose_a",
+                        loc: new vec(7, -6, 67),
+                        rot: new rot(0, 1.2)
+                    },
+                    {
+                        actorID: "elf3_pose_neutral",
+                        loc: new vec(19, -3, 44),
+                        rot: new rot(0.7, 0.3)
+                    },
+                ]
+            },
+            // Shot 3 - 1 - LS pull out changeling in crib
+            {
+                keys: [
+                    {
+                        actorID: "camera",
+                        loc: new vec(36, 64, 113),
+                        rot: new rot(-0.9)
+                    },
+                    {
+                        actorID: "changeling",
+                        loc: new vec(26, 9, 105),
+                        rot: new rot(-1.4, 0, -0.8),
+                        visible: true
+                    },
+                    {
+                        actorID: "elf_hand",
+                        loc: new vec(30, 32, 66),
+                        rot: new rot(0, -1.8, 0.8),
+                        visible: true
+                    },
+                    {
+                        actorID: "elf2_pose_a",
+                        loc: new vec(25, 32, 70),
+                        rot: new rot(1.5, -0.2, -0.6)
+                    }
+                ]
+            },
+            // Shot 4 - 1 - LS low angle of mother enter room
+            {
+                keys: [
+                    {
+                        actorID: "camera",
+                        loc: new vec(36, 30, 112),
+                        rot: new rot(0.2)
+                    },
+                    {
+                        actorID: "mother_pose_neutral",
+                        loc: new vec(-7, 0, -182),
+                        rot: new rot(0, 0.2),
+                        visible: true
+                    },
+                    hide("elf1_pose_neutral"),
+                    hide("elf2_pose_a"),
+                    hide("elf3_pose_neutral"),
+                    hide("elf_hand")
+                ]
+            }
         ];
 
         if (this.noLogo) {
