ba7498c55e511a94268b08c244c9ebc03ed73d2f
David Kanekanian <dkanekanian1@gmail.com>
2020-09-19 17:29:31 +0100

    Add data for shot 1 shell

--- src/timelines/two-hunchbacks-3704.ts
+++ src/timelines/two-hunchbacks-3704.ts
@@ -8,21 +8,34 @@ const rot = THREE.Euler;
 const hide = (actorID: string) => ({ actorID, visible: false });
 const show = (actorID: string) => ({ actorID, visible: true });
 
-export const getTimelineShots = (world: TwoHunchbacksWorld): TimelinePoint[] =>
-[
+export const getTimelineShots = (world: TwoHunchbacksWorld): TimelinePoint[] => [
     // initial position: shots A and B
     {
         keys: [
             {
                 actorID: "camera",
-                loc: new loc(),
-                rot: new rot(),
-                onVisit: () => {console.log("the first shot has begun!")}
+                loc: new loc(0, 100, 200),
+                onVisit: () => { console.log("the first shot has begun!") }
+            },
+            {
+                actorID: "backdrop_forest",
+                loc: new loc(44, 99, -260)
             },
             // hide the actors that come in later
-            // hide("elf1_pose_neutral"),
-            // hide("elf2_pose_neutral"),
-            // hide("elf3_pose_neutral"),
+            hide("tree"),
+            hide("elf1_pose_neutral"),
+            hide("elf2_pose_neutral"),
+            hide("elf3_pose_neutral"),
         ]
     },
+    // Shot 1 - LS of 2 brothers sat inside eating
+    {
+        keys: [
+            {
+                actorID: "camera",
+                loc: new loc(),
+                rot: new rot()
+            },
+        ]
+    }
 ];
